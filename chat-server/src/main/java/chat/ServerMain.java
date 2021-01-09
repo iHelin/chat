27cb9f686350cb2917f17e1301da344cd4ad522a
iHelin <ihelin@outlook.com>
2021-01-09 15:37:20 +0800

    update

--- chat-server/src/main/java/chat/ServerMain.java
+++ chat-server/src/main/java/chat/ServerMain.java
@@ -1,27 +1,12 @@
 package chat;
 
 import chat.project.netty.NettyServer;
-import org.apache.commons.cli.*;
 
 //启动服务端
 public class ServerMain {
 
-    private static int port = 8888;
-
-    public static void main(String[] args) throws ParseException {
-        CommandLineParser parser = new DefaultParser();
-        Options options = new Options();
-        options.addOption("h", "help", false, "Print this usage information");
-        options.addOption("p", "port", true, "Set server port.");
-        CommandLine commandLine = parser.parse(options, args);
-
-        if (commandLine.hasOption('h')) {
-            System.out.println("Use -p to set port.");
-            System.exit(0);
-        }
-        if (commandLine.hasOption('p')) {
-            port = Integer.parseInt(commandLine.getOptionValue('p'));
-        }
+    public static void main(String[] args) {
+        int port = 8765;
         NettyServer.start(port);
     }
 }
